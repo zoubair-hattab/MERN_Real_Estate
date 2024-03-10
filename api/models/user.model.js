3297df423af5a59d1f30823b1711f3d3164a6b00
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-03-10 13:53:35 +0100

    user.model added

---
+++ api/models/user.model.js
@@ -0,0 +1,33 @@
+import monggose from 'mongoose';
+
+const userSchema = new monggose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userImage: {
+      type: String,
+      default:
+        'https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const User = monggose.model('User', userSchema);
+export default User;
